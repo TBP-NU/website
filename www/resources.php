41dda0be470d4324ed6917bfc9f9f58b80704339
kimhj14 <heekim2016@u.northwestern.edu>
2015-04-19 15:51:49 -0500

    Create resources.php

---
+++ www/resources.php
@@ -0,0 +1,34 @@
+<?php
+	include "head.php";
+	include "header.php"; ?>
+	
+<div class="resources">
+	<div class="row">
+    	<div class="large-12">
+			<br>
+			<h1>Resources</h1>
+			<h4>Free online courses provided by <a href="https://www.udemy.com">Udemy</a></h4>
+		</div>
+		<br>
+	</div>
+	<div class="row">
+		<div class="large-6 medium-6 small-12 columns">
+			<h3 style="text-align:left;">CS courses</h3>
+				<li><a href="https://www.udemy.com/courses/Development/Web-Development/?p=1&price=price-free&view=grid">Web Development Courses</a></li>
+				<li><a href="https://www.udemy.com/courses/Development/Programming-Languages/?p=1&price=price-free&view=grid">Programming Language Courses</a></li>
+				<li><a href="https://www.udemy.com/courses/Development/Mobile-Apps/?p=1&price=price-free&view=grid">Mobile App Courses</a></li>
+				<li><a href="https://www.udemy.com/topic/java#article">The Pong Project</a></li>
+		</div>
+		<div class="large-6 medium-6 small-12 columns">
+			<h3 style="text-align:left;">Business courses</h3>
+				<li><a href="https://www.udemy.com/courses/Business/Entrepreneurship/?p=1&price=price-free&view=grid">Entrepreneurship Courses</a></li>
+				<li><a href="https://www.udemy.com/courses/Business/Management/?p=1&price=price-free&view=grid">Management Courses</a></li>
+				<li><a href="https://www.udemy.com/courses/Business/Project-Management/?p=1&price=price-free&view=grid">Project Management Courses</a></li>
+				<li><a href="https://www.udemy.com/courses/Personal-Development/Leadership/?p=1&price=price-free&view=grid">Leadership Courses</a></li>
+		</div>
+	</div>
+</div>
+
+<?php
+	include "footer.php";
+?>
