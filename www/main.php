003783298dbf051330c7d0e89db6fdaafca87031
kimhj14 <heekim2016@u.northwestern.edu>
2015-04-19 16:22:51 -0500

    Update main.php

--- www/main.php
+++ www/main.php
@@ -35,7 +35,7 @@
         <div class="large-12">
           <hr><br>
           <div class="about-tbp">
-            <h1>About Tau Beta Pi</h1>
+            <h1>About <span style="color:#0E63B3;">Tau Beta Pi</span></h1>
             <p></p><!--for space instead of br-->
             <p>Tau Beta Pi is the premier national engineering honor society in the country (est. 1885), and one of the most nationally recognized societies. Tau Beta Pi is committed to scholarship, personal character, and service to the profession, community and university. Every semester, we invite the top fifth of BS seniors and the top eighth of BS juniors in McCormick Engineering.</p>
             <p>At the core of the Society lie the qualities of distinguished scholarship and exemplary character, and for the past 70 years, the members of Illinois Gamma have not failed to confer honor upon their Alma Mater. Northwestern has produced leaders in engineering, government, the arts and sciences, and academia, to name a few, and the Northwestern Chapter of TBP exists to bring these like-minded individuals together to create a sum that is greater than the whole of its parts. One of the results has been to organize advising sessions and firesides inside dorms to better inform undergraduates in the process of selecting majors. And there’s much more to go!</p>
