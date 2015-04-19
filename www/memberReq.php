60669bef0f1e4d36bf72907d3a41aad8c54b9639
kimhj14 <heekim2016@u.northwestern.edu>
2015-04-19 15:52:08 -0500

    Create memberReq.php

---
+++ www/memberReq.php
@@ -0,0 +1,43 @@
+<?php
+	include "head.php";
+	include "header.php"; ?>
+	
+<div class="memberReq">
+	<div class="row">
+    	<div class="large-12">
+			<br>
+			<h1>Member Requirements</h1>
+			<p style="text-align: center;">(Last updated April 19, 2015)</p>
+			<br>
+			<h5>The New Exec Board of Tau Beta Pi seeks to increase the presence and recognition of this nationally recognized organization at Northwestern. The reward to fulfilling these requirements include networking with the brightest minds on campus, career development opportunities, and personal satisfaction from serving the Northwestern and Chicagoland communities.</h5>
+			<br>
+			<h4>Effective: Spring 2015</h4>
+			<ul>
+				<li>(Original) requirement is 2 service events per year & 1 social event per quarter.</li>
+				<li>The requirement above will remain for the remainder of the school year.</li>
+				<li>If you missed the mandatory meeting without e-mailing a valid reason to the exec board before the meeting, you are required to attend an extra event (either social or service) this quarter in place of the meeting. If you had an emergency and couldn’t attend the meeting at the last minute, send us an e-mail ASAP. </li>
+				<li>Attending a full 2-hour block of study hour can count as a social event for this quarter. </li>
+				<li>If you participated in one of the Pi Day Run committees, you are exempt from service requirements for this school year.</li>
+				<li>YOU WILL NOT GET KICKED OUT OF TBP BY NOT FULFILLING THE REQUIREMENTS.</li>
+				<li>If you successfully fulfill these requirements, you receive your graduation stole & cord for FREE ($40 otherwise; getting the graduation stole & cord is optional.)</li>
+			</ul>
+			<br>
+			<h4>Effective: Fall 2015</h4>
+			<ul>
+				<li>Minimum Number of Events: 2/quarter (social or service)</li>
+				<li>Mandatory General Meeting: 1/quarter</li>
+				<li>Attendance Policy: Email tbp@northwestern.edu before the meeting with a valid reason. The consequence for missing a general meeting is 1 extra event.</li>
+				<li>Requirements carry over each quarter.</li>
+				<li>If you volunteer to help with the logistics of an event (aka be on a “committee”), the 2-event requirement is fulfilled.</li>
+				<li>Attending a full 2-hour block of study hour can count as ONE of the 2 events for the quarter.</li>
+				<li>YOU WILL NOT GET KICKED OUT OF TBP BY NOT FULFILLING THE REQUIREMENTS.</li>
+				<li>If you successfully fulfill these requirements, you receive your graduation stole & cord for FREE ($40 otherwise; getting the graduation stole & cord is optional.)</li>
+			</ul>
+			<br>
+		</div>
+	</div>
+</div>
+
+<?php
+	include "footer.php";
+?>
