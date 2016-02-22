9e32340ea6d4c1d05b56d9881ccf277a54827687
heyyeh12 <heyyeh12@aol.com>
2016-02-22 07:23:21 -0600

    updated Pi Day Run website - live registration

--- www/piDayRun.php
+++ www/piDayRun.php
@@ -12,37 +12,51 @@
                 <img src="img/slideshow/pidayrun2.jpg" width="75%">
             </center>
             <br> 
-            <h4><span style="color:#0E63B3;font-weight:bold;">When:</span> Saturday, March 12th @ 9 am</h4>
-            <h4><span style="color:#0E63B3;font-weight:bold;">Where:</span> <a href="https://goo.gl/maps/Ldi72QAHMyw">NU Lakefill</a> (start / finish)</h4>
-            <br>              
+            <h4><span style="color:#0E63B3;font-weight:bold;">When:</span> Saturday, March 12th @ 9:30 A.M. (Check in @ 8:30 A.M.)</h4>
+            <h4><span style="color:#0E63B3;font-weight:bold;">Where:</span> <a href="https://www.google.com/maps/dir//42.0570501,-87.6705347/@42.0548043,-87.6765142,15.72z/data=!4m2!4m1!3e2">NU Lakefill</a> (Start / Finish)</h4>             
+            <br>
         </div>
     </div>
     <div class="row">
-        <!-- <a href="https://squareup.com/store/pi-day-run"> -->
-        <div class="panel callout center">
-            <h5>Registration &amp; race details coming soon!</h5>
-        </div>
-        <!-- </a> -->
-        <h2>About</h2>
+        <h2>Our Charity</h2><hr>
             <p> The Pi Day Run is a 3.14 mile (~5K) race in celebration of &pi; Day organized by the IL Gamma chapter of Tau Beta Pi, the honors engineering society. This year, <b>100% of proceeds will be donated to support Brave Initiatives</b>.</p> 
-            <p> <a href="http://www.braveinitiatives.com/">Brave Initiatives</a> is on a mission to empower high school girls to be agents of change in the world through design thinking, coding skills, and leadership training. 
+            <p> Brave Initiatives is on a mission to empower high school girls to be agents of change in the world through design thinking, coding skills, and leadership training. 
                 BraveCamps help girls learn front-end design and coding skills to develop solutions that better their communities, and ultimately, the world.</p>
-            <center>
-                <a href="http://www.braveinitiatives.com/"><img src="img/piDay/BraveCamp_logo.jpeg" width="25%"></a>
-            </center>
-
-        <!-- <h2>Registration</h2>
-            <p>  <a href="https://squareup.com/store/pi-day-run">https://squareup.com/store/pi-day-run</a></p>
-    
-            <p> Schedule <p>
-
-            <p>Check out <a href="http://tbp.mccormick.northwestern.edu/photo.php#nanogallery/gallery/6133745722049462929">photos from last year's run</a>.</p>
+            </p>Learn more at <a href="http://www.braveinitiatives.com/">www.braveinitiatives.com</a></p>
             <br>
-            <div class="responsive-iframe-container">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2962.407776688486!2d-87.67524753499767!3d42.05587811224978!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fda9d9c61b8ab%3A0x45b9fd9941874794!2sThe+Lakefill!5e0!3m2!1sen!2sus!4v1453079658086" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div>
+    <div class="row">
+        <h2>Registration</h2><hr>
+            <h5><b>STEP 1</b> - Fill out the <a href='http://goo.gl/forms/cdSqhGzvjc'> Google Form. </a></p>
+            <h5><b>STEP 2</b> - Pay registration fee. </h5>
+            <br>
+                <a href='https://mkt.com/pi-day-run' class='sq-embed-menu' data-menu-item-images='small' data-menu-accent-color='0b42b7' data-menu-template='column' data-menu-border='show' data-menu-item-descriptions='show' >Order Online</a>
+                <script src="https://cdn.sq-api.com/market/embed.js" charset="utf-8"></script>
+            <br>
+    </div>
+    <div class="row">
+        <h2> Race Details </h2><hr>
+            <div class="large-6 sm-12 columns">
+            <h5><b>LOCATION</b></h5>
+                <center>
+                <a href="https://www.google.com/maps/dir//42.0570501,-87.6705347/@42.0548043,-87.6765142,15.72z/data=!4m2!4m1!3e2">
+                    <img src="img/PiDay/StartEndMap.png" width="80%">
+                </a>
+                </center>
             </div>
-            <br>
-        </div> -->
+            <div class="large-6 sm-12 columns">
+            <h5><b>SPONSORED BY</b></h5>
+                <center>
+                <img src="img/PiDay/wildIdeaLogo.png" width="40%">
+                </center>
+            <!-- <p> Check out <a href="http://tbp.mccormick.northwestern.edu/photo.php#nanogallery/gallery/6133745722049462929">photos from last year's run here</a>.</p> -->
+            </div>
+    </div>
+    <br>
+    <div class="row">
+        <h2>Questions?</h2><hr>
+            <p> Please e-mail <a href="mailto:tbp@northwestern.edu">tbp@northwestern.edu</a> if you have any questions or concerns.
+            
     </div>
 </div>
 
